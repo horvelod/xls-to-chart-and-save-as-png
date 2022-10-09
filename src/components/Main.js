49eb74cde5b1bc804af2e8916e1722f926bec3f6
“Elod <hoelod@outlook.com>
2022-10-09 11:23:29 +0200

    README edited

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,39 +1,44 @@
 import { BarChart, FileDownload, UploadFile } from '@mui/icons-material';
-import { Avatar, Button, Card, CardContent, CardHeader, Grid } from '@mui/material';
+import { Avatar, Button, Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
 import { blue, green, pink } from '@mui/material/colors';
 import { Link } from 'react-router-dom';
 
 
 function Mainx() {
 
-  return <Grid container spacing={2} sx={{mt: 10}}>
+  return <Grid container spacing={2} sx={{ mt: 10 }}>
+    <Grid item xs={12} md={12} sx={{display: 'flex', justifyContent: 'center', mb: 4}}>
+    <Typography variant="h4" component="h1">
+      Xls to chart and save chart as png image
+    </Typography>
+    </Grid>
     <Grid item xs={12} md={4}>
-      <Card variant="outlined" sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <CardHeader avatar={<Avatar sx={{bgcolor: blue[500]}}>1</Avatar>} title="Select a chart type!"/>
+      <Card variant="outlined" sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <CardHeader avatar={<Avatar sx={{ bgcolor: blue[500] }}>1</Avatar>} title="Select a chart type!" />
         <CardContent>
-          <BarChart sx={{fontSize: '120px', color: blue[500]}} />
+          <BarChart sx={{ fontSize: '120px', color: blue[500] }} />
         </CardContent>
       </Card>
     </Grid>
     <Grid item xs={12} md={4}>
-    <Card variant="outlined" sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <CardHeader avatar={<Avatar sx={{bgcolor: pink[500]}}>1</Avatar>} title="Choose your data file(xls, xlsx or csv)! The program do not save or store your file!"/>
+      <Card variant="outlined" sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <CardHeader avatar={<Avatar sx={{ bgcolor: pink[500] }}>1</Avatar>} title="Choose your data file(xls, xlsx or csv)! The program do not save or store your file!" />
         <CardContent>
-          <UploadFile sx={{fontSize: '120px', color: pink[500]}} />
+          <UploadFile sx={{ fontSize: '120px', color: pink[500] }} />
         </CardContent>
       </Card>
     </Grid>
     <Grid item xs={12} md={4}>
-    <Card variant="outlined" sx={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <CardHeader avatar={<Avatar sx={{bgcolor: green[500]}}>1</Avatar>} title="Download your chart as png!"/>
+      <Card variant="outlined" sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <CardHeader avatar={<Avatar sx={{ bgcolor: green[500] }}>1</Avatar>} title="Download your chart as png!" />
         <CardContent>
-          <FileDownload sx={{fontSize: '120px', color: green[500]}} />
+          <FileDownload sx={{ fontSize: '120px', color: green[500] }} />
         </CardContent>
       </Card>
     </Grid>
-    <Grid item xs={12} display="flex" justifyContent="center" alignItems="center" sx={{mt: 6}}>
+    <Grid item xs={12} display="flex" justifyContent="center" alignItems="center" sx={{ mt: 6 }}>
       <Button component={Link} to="/data-visualisation" variant="contained" size="large">Get started</Button>
-      </Grid>
+    </Grid>
   </Grid>
 
 
